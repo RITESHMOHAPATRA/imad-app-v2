fa33ce55b84b353aefb69edeec54fbb847033f45
RITESH KUMAR MOHAPATRA <mohapatraritesh97@gmail.com>
2017-03-24 12:47:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,17 +1,117 @@
-var express = require('express');
-var morgan = require('morgan');
+var express = require('express'); //to create the web server
+var morgan = require('morgan');   //to output log of our servers
 var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
 
+var articles = {
+    'article-one': {
+    	title: "Article One | Shubham Rath",
+    	heading: "Article One",
+    	date: "March 11, 2017",
+    	content: `
+    	<p>
+    	This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.
+    	</p>
+    	<p>
+    	This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.
+    	</p>
+    	<p>
+    	This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.
+    	</p>
+    	`
+    },
+    'article-two': {
+        title: "Article Two | Shubham Rath",
+    	heading: "Article Two",
+    	date: "March 11, 2017",
+    	content: `
+    	<p>
+    	This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.
+    	</p>
+    	<p>
+    	This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.
+    	</p>
+    	<p>
+    	This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.
+    	</p>
+    	`
+    },
+    'article-three': {
+        title: "Article Three | Shubham Rath",
+    	heading: "Article Three",
+    	date: "March 11, 2017",
+    	content: `
+    	<p>
+    	This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.
+    	</p>
+    	<p>
+    	This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.
+    	</p>
+    	<p>
+    	This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.This is the paragraph.
+    	</p>
+    	`
+    }
+};
+
+function createTemplate(data) {
+	var title = data.title;
+	var heading = data.heading;
+	var date = data.date;
+	var content = data.content;
+
+	var htmlTemplate = `
+    	<!DOCTYPE html>
+    	<html>
+    	<head>
+    		<title>${title}</title>
+    		<meta name="viewport" content="width-device-width, initial-scale=1"/>
+    		<link href="/ui/style.css" rel="stylesheet" />
+    	</head>
+    
+    	<body>
+    		<div class="container">
+    			<div>
+    				<a href="/">Home</a>
+    			</div>
+    			<hr/>
+    			<h3>
+    				${heading}
+    			</h3>
+    			<div>
+    				${date}
+    			</div>
+    			<div>
+    				${content}
+    			</div>
+    		</div>
+    	</body>
+    	</html>
+    	`;
+	return htmlTemplate;
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/:articleName', function (req, res) {
+    //articleName == article-one
+    var articleName = req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
